c9dda33dbb20f8e69d257b601147c0afd64b7263
imcuky <catherine.lee.le@gmail.com>
2023-03-20 00:24:17 -0400

    update

--- SQL/eHotels-DDL.sql
+++ SQL/eHotels-DDL.sql
@@ -1,11 +1,31 @@
-drop table hotelchain;
-drop table hotelchainphone;
-drop table hotelchainemail;
-drop table hotel;
-drop table hotelphone;
+drop table amenity;
+drop table problem;
+drop table room;
+drop table roominfo;
+
+drop table customerphone;
+drop table customer;
+
+drop table employeephone;
+drop table employeeposition;
 drop table employee;
 
-create table HotelChain(
+drop table hotelphone;
+drop table hotel;
+
+drop table hotelchainphone;
+drop table hotelchainemail;
+drop table hotelchain;
+
+
+
+
+
+
+
+
+
+create table if not exists HotelChain(
 hotel_chain_code CHAR(4) PRIMARY KEY,
 name VARCHAR(50) NOT NULL,
 num_of_hotels INTEGER NOT NULL,
@@ -15,14 +35,14 @@ hq_state_prov VARCHAR(50) NOT NULL,
 hq_zip VARCHAR(6) NOT NULL
 );
 
-create table HotelChainPhone(
+create table if not exists HotelChainPhone(
 hotel_chain_code CHAR(4),
 phone_number CHAR(11),
 FOREIGN KEY(hotel_chain_code)REFERENCES HotelChain(hotel_chain_code) on delete cascade,
 primary key (hotel_chain_code, phone_number)
 );
 
-create table HotelChainEmail(
+create table if not exists HotelChainEmail(
 hotel_chain_code CHAR(4),
 email VARCHAR(50),
 FOREIGN KEY(hotel_chain_code)REFERENCES HotelChain(hotel_chain_code) on delete cascade,
@@ -30,7 +50,7 @@ primary key(hotel_chain_code,email )
 );
 
 
-create table Hotel(
+create table if not exists Hotel(
 hotel_chain_code CHAR(4),
 hotel_code INTEGER ,
 name VARCHAR(50) NOT NULL,
@@ -46,7 +66,7 @@ FOREIGN KEY (hotel_chain_code) REFERENCES HotelChain(hotel_chain_code) on delete
 primary key(hotel_chain_code, hotel_code)
 );
 
-create table HotelPhone(
+create table if not exists HotelPhone(
 hotel_chain_code CHAR(4),
 hotel_code INTEGER,
 phone_number CHAR(11),
@@ -54,8 +74,8 @@ FOREIGN KEY(hotel_chain_code, hotel_code)REFERENCES Hotel(hotel_chain_code, hote
 primary key(hotel_chain_code, hotel_code, phone_number)
 );
 
-create table Employee(
-emp_ID INTEGER PRIMARY KEY,
+create table if not exists Employee(
+emp_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
 f_name VARCHAR(50) NOT NULL,
 l_name VARCHAR(50) NOT NULL,
 ssn INTEGER NOT NULL UNIQUE CHECK (ssn BETWEEN 0 AND 999999999),
@@ -70,14 +90,14 @@ FOREIGN KEY(hotel_chain_code, hotel_code) REFERENCES Hotel(hotel_chain_code, hot
 
 );
 
-create table EmployeePhone(
+create table if not exists EmployeePhone(
 emp_ID INTEGER,
 phone_number CHAR(11),
 FOREIGN KEY (emp_id) REFERENCES Employee(emp_ID),
 primary key (emp_ID, phone_number)
 );
 
-create table EmployeePosition(
+create table if not exists EmployeePosition(
 emp_ID INTEGER ,
 position VARCHAR(50),
 FOREIGN KEY (emp_id) REFERENCES Employee(emp_ID),
@@ -85,8 +105,8 @@ primary key (emp_ID, position)
 );
 
 
-create table Customer(
-cust_ID INTEGER PRIMARY KEY,
+create table if not exists Customer(
+cust_ID INTEGER PRIMARY KEY AUTO_INCREMENT,
 f_name VARCHAR(50) NOT NULL,
 l_name VARCHAR(50) NOT NULL,
 ssn INTEGER NOT NULL UNIQUE CHECK (ssn BETWEEN 0 AND 999999999),
@@ -97,7 +117,7 @@ email VARCHAR(50) NOT NULL,
 registration_date DATETIME NOT NULL
 );
 
-create table CustomerPhone(
+create table if not exists CustomerPhone(
 cust_ID INTEGER,
 phone_number CHAR(11),
 FOREIGN KEY(cust_ID)REFERENCES Customer(cust_ID),
@@ -105,7 +125,7 @@ primary key(cust_ID, phone_number)
 );
 
 
-create table RoomInfo(
+create table if not exists RoomInfo(
 room_info_no INTEGER PRIMARY KEY,
 hotel_chain_code CHAR(4) NOT NULL,
 hotel_code INTEGER NOT NULL,
@@ -120,7 +140,7 @@ CHECK (status IN ('Available', 'Occupied', 'In renovation', 'Deleted'))
 );
 
 
-create table Room(
+create table if not exists Room(
 hotel_chain_code CHAR(4),
 hotel_code INTEGER,
 room_no INTEGER,
@@ -131,14 +151,14 @@ primary key(hotel_chain_code, hotel_code,room_no)
 );
 
 
-create table Amenity(
+create table if not exists Amenity(
 room_info_no INTEGER,
 amenity VARCHAR(50),
 FOREIGN KEY (room_info_no) REFERENCES RoomInfo(room_info_no),
 primary key(room_info_no, amenity)
 );
 
-create table Problem(
+create table if not exists Problem(
 room_info_no INTEGER,
 title VARCHAR(50),
 description VARCHAR(50),
--- SQL/eHotels-data-insertion.sql
+++ SQL/eHotels-data-insertion.sql
@@ -18,3 +18,29 @@ insert into hotel values
                     ('HILN',1007, "Hilton Hawaiian Village Waikiki Beach Resort", -1, 4, 5, "2005 Kalia Rd", "Honolulu", "Hawaii", "96815", "info@hiltonhawaiianvillage.com"),
                     ('HILN',1008, "Hilton Los Cabos Beach & Golf Resort", -1, 4, 5, "Carretera Transpeninsular Km 19.5", "Los Cabos", "California", "23447", "info@hiltonloscabos.com");
                     
+insert into employee values
+			(1000, "John", "Smith", 123456789, "123 Main St.", "San Francisco", "California", "94102", "john.smith@email.com",'HILN',  1001);
+ insert into employee(f_name,l_name,ssn,number_street,city,state_prov,zip,email,hotel_chain_code,hotel_code) values           
+            ("Samuel", "Curtis", 145236852, "3883 Natoque Rd.", "Beverly Hills", "California", "90210","cras.dolor.dolor@hotmail.ca",'HILN',  1002),
+			  ("Aileen", "Fisher",120254863,"9020 Eros Av.","Chicago", "Illinois", "60611","integer.vitae@google.edu",'HILN', 1003),
+			  ("Armando", "Macdonald",254875410,"7876 Cras Av." ,"New York", "NY", "10282","proin.non.massa@outlook.net",'HILN', 1004),
+			  ("Kelsie", "Henderson",745120369,"355-1941 Primis Av.","New Orleans", "Louisiana", "70130", "morbi@yahoo.com",'HILN', 1005),
+			  ("Wallace", "White",424158632,"426 Ullamcorper St.","Orlando", "FL", "32821","lorem.vehicula@icloud.couk",'HILN', 1006),
+              ("Dorian", "Hoover",854120365,"5042 Luctus Road", "Honolulu", "Hawaii", "96815","tempus.non@yahoo.net",'HILN',1007),
+				("Orli", "Ortiz",632104852,"9419 Venenatis St.","Los Cabos", "California", "23447","dignissim@yahoo.couk",'HILN',1008);
+
+update hotel set manager_ID = 1000 where hotel_code = 1001;
+update hotel set manager_ID = 1001 where hotel_code = 1002;
+update hotel set manager_ID = 1002 where hotel_code = 1003;
+update hotel set manager_ID = 1003 where hotel_code = 1004;
+update hotel set manager_ID = 1004 where hotel_code = 1005;
+update hotel set manager_ID = 1005 where hotel_code = 1006;
+update hotel set manager_ID = 1006 where hotel_code = 1007;
+update hotel set manager_ID = 1007 where hotel_code = 1008;
+
+
+
+
+
+
+            
